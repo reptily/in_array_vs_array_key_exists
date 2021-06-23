e1667298811adae34c807d82186754162f1aa490
Аристархов Владимир <v.aristarkhov@rabota.ru>
2021-06-23 08:44:02 +0300

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+php test.php
+```
+
+Test performance testInArray took 0.00253 seconds
+Test performance testArrayKeyExists took 0.00000 seconds
---
+++ test.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types = 1);
+
+/** @var int Количество элементов в массиве */
+const COUNT_ITEM = 1000000;
+const SEARCH_ITEM = 999999;
+
+$inArray = [];
+$arrayKeyExists = [];
+
+if (SEARCH_ITEM > COUNT_ITEM) {
+    printf("Error: SEARCH_ITEM is greater than COUNT_ITEM");
+    exit();
+}
+
+function testPerformance(string $name, Closure $closure): void
+{
+    $start = microtime(true);
+    $closure();
+    $end = microtime(true);
+    printf("Test performance %s took %.5f seconds\n", $name, $end - $start);
+}
+
+for ($i = 0; $i < COUNT_ITEM; $i++) {
+    $inArray[] = $i;
+    $arrayKeyExists[$i] = true;
+}
+
+$testInArray = function () use ($inArray) {
+    in_array(SEARCH_ITEM, $inArray);
+};
+
+$testArrayKeyExists= function () use ($arrayKeyExists) {
+    array_key_exists(SEARCH_ITEM, $arrayKeyExists);
+};
+
+testPerformance('testInArray', $testInArray);
+testPerformance('testArrayKeyExists', $testArrayKeyExists);
